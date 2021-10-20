a9a8a5bbb109dd1eeb06346ff6f6f4ae8a672a2e
Johannes Hanuja <hanujajohannes@gmail.com>
2021-10-20 20:54:34 +0200

    saving file with popup on Android

--- src/persistance.cpp
+++ src/persistance.cpp
@@ -16,9 +16,19 @@ Persistance::Persistance(QObject *parent) :
 Persistance::~Persistance(){}
 
 bool Persistance::isFilenameValid() const{
-    QFileInfo layoutFileInfo{mFilename.toLocalFile()};
-    bool isValid = layoutFileInfo.exists() && layoutFileInfo.isFile() && layoutFileInfo.isWritable() && 0 == layoutFileInfo.suffix().compare("json", Qt::CaseInsensitive);
-    return isValid;
+    if(Util::isMobileDevice()){
+            Util::checkAndRequestPermission();
+            qDebug() << mFilename.fileName();
+            QString androidFilePath = QString("%1/%2").arg("/storage/emulated/0/QtRobo/", mFilename.fileName());
+            QFileInfo layoutFileInfo{QFile(androidFilePath)};
+            bool isValid = layoutFileInfo.exists() && layoutFileInfo.isFile() && layoutFileInfo.isWritable() && 0 == layoutFileInfo.suffix().compare("json", Qt::CaseInsensitive);
+            qDebug() << "Is valid: " << isValid;
+            return isValid;
+        }else{
+            QFileInfo layoutFileInfo{mFilename.toLocalFile()};
+            bool isValid = layoutFileInfo.exists() && layoutFileInfo.isFile() && layoutFileInfo.isWritable() && 0 == layoutFileInfo.suffix().compare("json", Qt::CaseInsensitive);
+            return isValid;
+        }
 }
 
 QUrl Persistance::filename() const{
@@ -30,14 +40,32 @@ void Persistance::filename(const QUrl& filename){
     emit filenameValidChanged(isFilenameValid());
 }
 
+bool Persistance::qtRoboFolderSelectedOnMobile() {
+    #ifdef Q_OS_ANDROID
+    QSettings settings;
+    return settings.value("openedFile").toInt() == 1;
+    #endif
+}
+
 void Persistance::restore(){
     qDebug() << "R: " << mFilename << mFilename.fileName();
 
     auto layoutFile = [&]()->QFile{
         if(Util::isMobileDevice()){
+            #ifdef Q_OS_ANDROID
             Util::checkAndRequestPermission();
-            const QString androidFilePath = QString("%1/%2%3").arg(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation), mFilename.fileName().split(":").last(), ".json");
+            const QString androidFilePath = QString("%1/%2").arg("/storage/emulated/0/QtRobo/", mFilename.fileName());
+            QFileInfo info = QFileInfo(androidFilePath);
+
+
+            QSettings settings;
+            if (info.exists()){
+                settings.setValue("openedFile",1);
+            }
+            qDebug() << settings.value("openedFile").toInt();
+
             return QFile{androidFilePath};
+            #endif
         }else{
             return QFile{mFilename.toLocalFile()};
         }
@@ -45,7 +73,6 @@ void Persistance::restore(){
 
     layoutFile.open(QIODevice::ReadOnly);
 
-    qDebug() << "Android Path(" << layoutFile.isWritable() << "):" << layoutFile.fileName();
 
     if(layoutFile.isOpen() && layoutFile.isReadable()){
         qDebug() << "Is open";
@@ -81,9 +108,16 @@ void Persistance::persist(){
 
     auto layoutFile = [&]()->QFile{
         if(Util::isMobileDevice()){
+            #ifdef Q_OS_ANDROID
             Util::checkAndRequestPermission();
-            const QString androidFilePath = QString("%1/%2").arg(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation), mFilename.fileName().split(":").last());
+
+            QDir dir("/storage/emulated/0/QtRobo");
+            if (!dir.exists())
+                dir.mkpath(".");
+
+            const QString androidFilePath = QString("%1/%2").arg("/storage/emulated/0/QtRobo", mFilename.fileName().split(":").last());
             return QFile{androidFilePath};
+            #endif
         }else{
             return QFile{mFilename.toLocalFile()};
         }
